4b2bd3cd33edaf7c01f8b1b3bc44655673169e7d
hashfriend <hashfriend@proton.me>
2025-10-27 00:55:58 +0000

    use astro-relative-links

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import mdx from '@astrojs/mdx'
 import { defineConfig, fontProviders } from 'astro/config'
+import relativeLinks from 'astro-relative-links'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), relativeLinks()],
   experimental: {
     fonts: [
       {
